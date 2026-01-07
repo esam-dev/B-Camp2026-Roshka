8a2bb2472c43efd5afc7545cdd8f12baa3f4192b
Elias Ortiz <elortiz@uaa.edu.py>
2026-01-07 10:07:45 -0300

    fix(ManoPoker): correcion de errores en el constructor y el metodo esEscalera

--- EjercicioPoker/src/poker/ManoPoker.java
+++ EjercicioPoker/src/poker/ManoPoker.java
@@ -9,7 +9,7 @@ public class ManoPoker {
 	private final List<Carta> cartas = new  ArrayList<>();
 	
 	// Constructor  que valida  y crea  la mano 
-	public ManoPoker(String[] codigoCartas) {
+	public ManoPoker(String[] codigosCartas) {
 		if (codigosCartas.length != 5 ) {
 			throw new IllegalArgumentException("Debe haber 5 cartas ");
 		}
@@ -21,6 +21,52 @@ public class ManoPoker {
 			cartas.add(new Carta(codigo));
 		}
 	}
-	
+	// Identifica  el tipo de mano 
+	public TipoMano identificarMano() {
+		Map<Integer, Integer> conteo = new HashMap<>();
+		Set<Palo> palos =  new  HashSet<>();
+		List<Integer> valores = new ArrayList<>();
+		
+		for (Carta c :  cartas) {
+			int v = c.getValor().getRango();
+			valores.add(v);
+			conteo.put(v, conteo.getOrDefault(v, 0) + 1 );
+			palos.add(c.getPalo());
+		}
+		Collections.sort(valores);
+		
+		boolean esColor = palos.size() == 1; 
+		boolean esEscalera = esEscalera(valores);
+		
+		if (esEscalera && esColor) return TipoMano.ESCALERA_COLOR;
+		if (conteo.containsValue(4)) return  TipoMano.POKER;
+		if (conteo.containsValue(3) && conteo.containsValue(2)) return TipoMano.FULL;
+		if (esColor) return TipoMano.COLOR;
+		if (esEscalera) return TipoMano.ESCALERA;
+		if (conteo.containsValue(3)) return TipoMano.TRIO;
+		
+		int pares = 0; 
+		for (int c : conteo.values()) {
+			if (c == 2) pares++;
+		}
+		
+		if (pares == 2)  return TipoMano.DOBLE_PAR;
+		if (pares == 1) return TipoMano.PAR;
+		
+		return TipoMano.CARTA_ALTA;		
+	}
+	// Verifica si los valores forman una escalera
+	private boolean esEscalera (List<Integer> valores) {
+		// Caso A-2-3-4-5
+		if (valores.equals(Arrays.asList(2, 3, 4, 5, 14 ))) {
+			return true;  
+		}
+		for (int  i = 0;  i < valores.size() -1 ; i++) {
+			if (valores.get(i) + 1  !=  valores.get(i + 1 )) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
