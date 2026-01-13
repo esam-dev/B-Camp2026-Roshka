6500e7afa6112613e25b975555ad80eb555fce13
Elias Ortiz <macpc@MacBook-Air-de-MACPC.local>
2026-01-13 15:17:44 -0300

    test(SQL) add Ejercicios de SQL - 3

--- SQL/Script-EjerciciosSQL.sql
+++ SQL/Script-EjerciciosSQL.sql
@@ -4,13 +4,12 @@ SELECT
 c.id,
 c.nombre,
 c.apellido,
-COUNT (f.id) as total_facturas
+COUNT (f.id) AS total_facturas
 FROM public.cliente c 
-JOIN public.factura f on f.cliente_id  = c.id 
-group by c.id , c.nombre , c.apellido 
-order by total_facturas desc;
-
-
+JOIN public.factura f ON f.cliente_id  = c.id 
+GROUP BY c.id , c.nombre , c.apellido 
+ORDER BY total_facturas desc
+limit 10;
 
 
 /*Ejercicio 2 - Top clientes que mas gastaron */
@@ -24,4 +23,29 @@ JOIN factura f ON F.cliente_id  = c.id
 GROUP BY c.id , c.nombre , c.apellido 
 ORDER BY  total_gastado  DESC
 LIMIT 10;
+
+
+/* Ejercicio 3 - Top monedas mas utilizadas */
+SELECT 
+   m.nombre  as moneda,
+   COUNT(f.id) AS veces_usada
+FROM factura f 
+JOIN moneda m ON  m.id = f.moneda_id 
+GROUP BY m.nombre
+ORDER BY veces_usada desc
+limit 10;
+
+
+/* Ejercicio  4 - Top proveedor de productos */
+select 
+   p.nombre as proveedor, 
+   COUNT(pr.id) as  total_productos 
+from proveedor  p 
+join producto pr on pr.proveedor_id  = p.id 
+group by p.nombre 
+order by total_productos desc
+limit 5;
+
+
+
 
