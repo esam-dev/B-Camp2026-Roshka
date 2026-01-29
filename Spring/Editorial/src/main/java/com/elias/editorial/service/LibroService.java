c592ef4ccd830cf5e29d936943774e397b16954e
Elias Ortiz <macpc@MacBook-Air-de-MACPC.local>
2026-01-29 15:56:36 -0300

    feat: agregado de interfaz LibroService

---
+++ Spring/Editorial/src/main/java/com/elias/editorial/service/LibroService.java
@@ -0,0 +1,13 @@
+package com.elias.editorial.service;
+
+import com.elias.editorial.dto.LibroDTO;
+import com.elias.editorial.model.Libro;
+import java.util.List;
+
+public interface LibroService {
+    Libro guararDesdeDTO(LibroDTO dto);
+    List<Libro> listarTodos();
+    Libro buscarPorId (Long id );
+    Libro actualizar (Long id , Libro libro);
+    void eliminar (Long id );
+}
