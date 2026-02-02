f9fb77f1dbcd3519846155605c3e318cb84715ed
Elias Ortiz <macpc@MacBook-Air-de-MACPC.local>
2026-02-02 18:19:35 -0300

    refactor:[security] add de JwtUtil

--- Spring/Login-JWT/src/main/java/com/example/demo/security/JwtUtil.java
+++ Spring/Login-JWT/src/main/java/com/example/demo/security/JwtUtil.java
@@ -5,21 +5,23 @@ import io.jsonwebtoken.security.Keys;                                           
 import org.springframework.stereotype.Component;                                       // bean Spring
 import java.nio.charset.StandardCharsets;                                               // UTF-8 charset
 import java.security.Key;                                                               // tipo Key
-import java.util.Base64;                                                                // decode base64
 import java.util.Date;                                                                  // fechas
 
 @Component
 public class JwtUtil {
 
-    // Clave secreta 
-    private static final String SECRET_KEY = " ";
-
     // Una hora de validez
     private static final long EXPIRATION_TIME = 1000 * 60 * 60 ; 
+    // Clave secreta 
+    private static final String SECRET = 
+          System.getenv("JWT_SECRET") != null
+                 ? System.getenv("JWT_SECRET")
+                 : "clave_secreta_dev_no_usar_en_prod_123456";
 
-    private Key  getKey(){
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
-    }
+      // Clave final usada para firmar y validar
+    private final Key key = Keys.hmacShaKeyFor(
+        SECRET.getBytes(StandardCharsets.UTF_8)
+      );
 
     // Genera el token 
     public String generarToken (String username ){
@@ -29,19 +31,28 @@ public class JwtUtil {
                 .setExpiration(
                         new Date(System.currentTimeMillis() + EXPIRATION_TIME)
                 )
-                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .signWith(getKey(), SignatureAlgorithm.HS256)  // firma 
                 .compact();
     }
 
-    // Extrae username desde el token 
+    // Extrae username del token 
     public String extraerUsername (String token ){
 
         return Jwts.parserBuilder()
                 .setSigningKey(getKey())
                 .build()
-                .parseClaimsJws(token)
+                .parseClaimsJws(token)      // Valida firma y expiracion 
                 .getBody()
                 .getSubject();
     }
 
+    // Valida token (True / false)
+    public boolean validarToken (String token ){
+        try{
+            extraerUsername(token);
+            return true;
+        } catch(JwtException e ){   
+            return false;
+        }
+    }
 }
